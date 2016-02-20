b292f8c163cbe2265cfbca7e1c1a5b16343cde0e
toanhtran <toanh.t.tran@gmail.com>
2016-02-20 18:58:08 +0900

    first commit

--- js/init.js
+++ js/init.js
@@ -1,8 +1,20 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.modal-trigger').leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+            ready: function () {
+                alert('Ready');
+            }, // Callback for Modal open
+            complete: function () {
+                    alert('Closed');
+                } // Callback for Modal close
+        });
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
